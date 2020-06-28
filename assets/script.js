562d1083bc22f062ad57372c478bd8c2c2f5c617
Jeorge Donato <jeorgekhenr@gmail.com>
2020-06-27 17:57:26 -0700

    Added script and style file

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+let quizObj = {
+  first: {
+    question: "What is love?",
+    choices: ["a", "b", "c", "d"],
+    answer: "a"
+  },
+  second: {
+    question: "Who's most likely to?",
+    choices: ["a", "b", "c", "d"],
+    answer: "b"
+  }
+};

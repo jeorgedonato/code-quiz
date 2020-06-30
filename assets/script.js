7c54b3e06162f052dc365814c33310b7cfcdd56e
Jeorge Donato <jeorgekhenr@gmail.com>
2020-06-30 10:22:36 -0700

    Added a function that check the user answer

--- assets/script.js
+++ assets/script.js
@@ -6,43 +6,44 @@ let randomNumPicker = () => {
 }
 //Random
 
-let quizObj = {
-  first: {
+let quizBank = [
+  {
+    id: 1,
     question: "What is the command that we can use to change directory in the terminal command line?",
     choices: ["mv", "rm", "history", "cd"],
     answer: "cd",
     type: "Multiple",
   },
-  second: {
+  {
+    id: 2,
     question: "What does SSH means?",
     choices: ["Secure Shell", "Secure Sheets", "Solid Shake", "Snake Sheets"],
     answer: "Secure Shell",
     type: "Multiple",
   },
-  third: {
+  {
+    id: 3,
     question: "It is a CSS position that is always relative to the parent",
     choices: ["Relative", "Static", "Fixed", "Absolute"],
     answer: "Relative",
     type: "Multiple",
   },
-  fourth: {
+  {
+    id: 4,
     question: "______ is a CSS property that is use to show an element on top of the another element",
     choices: ["margin", "z-index", "display", "visible"],
     answer: "z-index",
     type: "Multiple",
-  },
-  // fifth: {
-  //   question: "What is the syntax for commenting a line in Javascript?",
-  //   answer: "//",
-  //   type: "Identification"
-  // }
-};
+  }];
 
 let highScores = new Object;
 let quizArea = document.querySelector(".quiz-area");
 let currentScore = 0;
 let correctAnswers = 0;
 let wrongAnswers = 0;
+let answeredQuestions = 0;
+const startBtn = document.querySelector("#start-btn");
+const startDiv = document.querySelector(".start-btn-div");
 
 // Shuffle Array
 // Source https://www.rosettacode.org/wiki/Knuth_shuffle
@@ -67,50 +68,79 @@ let getRandomMotivation = () => {
 }
 //Random Motivation
 
-//Check the answer
-let answerChecker = id => {
-  const splittedAnswer = id.split("_");
-  let correctAnswer = quizObj[splittedAnswer[0]].answer;
-  const alertArea = document.querySelector("#alert-area");
-  // console.log(correctAnswer + " = " + splittedAnswer[1])
-  if (correctAnswer === splittedAnswer[1]) {
-    currentScore + 5;
-    correctAnswers++;
-    alertArea.innerHTML = `<div class="alert alert-success" role="alert">You're ${getRandomMotivation()}! Keep up the good work!</div>`;
-  } else {
-    wrongAnswers++;
-    alertArea.innerHTML = `<div class="alert alert-danger" role="alert">The answer is wrong! Come on now!</div>`;
+//Answer Checker
+quizArea.addEventListener("click", function (event) {
+  let target = event.target;
+
+  // console.log(`${answeredQuestions} - ${quizBank.length}`)
+  // if (answeredQuestions === quizBank.length) {
+  //   return;
+  // }
+  if (target.matches("li") === true) {
+    let arrId = target.getAttribute("data-id")
+    let choice = target.getAttribute("data-choice");
+    let index = target.getAttribute("data-index");
+    const correctArr = quizBank.find(element => element.id == arrId);
+    const correctAnswer = correctArr.answer;
+    const alertArea = document.querySelector("#alert-area");
+    if (correctAnswer === choice) {
+      currentScore += 5;
+      correctAnswers++;
+      alertArea.innerHTML = `<div class="alert alert-success" id="alert-clip" role="alert">You're ${getRandomMotivation()}! Keep up the good work!</div>`;
+      setTimeout(function () { document.getElementById("alert-clip").style.display = "none"; }, 600);
+    } else {
+      wrongAnswers++;
+      alertArea.innerHTML = `<div class="alert alert-danger" id="alert-clip" role="alert">The answer is wrong! Come on now!</div>`;
+      setTimeout(function () { document.getElementById("alert-clip").style.display = "none"; }, 600);
+    }
+    document.querySelector(`#question_${index}`).setAttribute("style", "display:none;");
+    document.querySelector(`#question_${parseInt(index) + 1}`).setAttribute("style", "display:block;");
+    // console.log(nextQuestion)
+    answeredQuestions++;
+    // console.log(`Score is ${currentScore}`)
   }
-};
-//Check the answer
+});
+//Answer Checker
+
 
 //Quiz Maker
 let quizMaker = () => {
-  // const shuffledQuiz = knuthShuffle(quizObj);
-  // console.log(shuffledQuiz)
-  let orderCounter = 1
-  for (const order in quizObj) {
+  const shuffledQuiz = knuthShuffle(quizBank);
+  for (let i = 0; i < shuffledQuiz.length; i++) {
     let questionDiv = document.createElement("div");
-    questionDiv.setAttribute("class", `"question_${orderCounter}"`)
-    questionDiv.textContent = quizObj[order].question;
+    questionDiv.setAttribute("id", `question_${i + 1}`);
+    questionDiv.setAttribute("style", "display:none;");
+    questionDiv.textContent = shuffledQuiz[i].question;
     quizArea.appendChild(questionDiv);
-    if (quizObj[order].type === "Multiple") {
+    if (shuffledQuiz[i].type === "Multiple") {
       let choiceUl = document.createElement("ul");
       choiceUl.setAttribute("class", "list-group")
-      let shuffleChoices = knuthShuffle(quizObj[order].choices);
-      for (let i = 0; i < shuffleChoices.length; i++) {
+      let shuffleChoices = knuthShuffle(shuffledQuiz[i].choices);
+      for (let j = 0; j < shuffleChoices.length; j++) {
         let choiceLi = document.createElement("li");
         choiceLi.setAttribute("class", "list-group-item");
-        choiceLi.innerHTML = `<button class="btn btn-primary" id="${order}_${quizObj[order].choices[i]}" onclick="answerChecker(this.id)">${shuffleChoices[i]}</button>`;
+        // let choiceBtn = document.createElement("button");
+        choiceLi.textContent = shuffleChoices[j];
+        choiceLi.setAttribute("class", "list-group-item list-group-item-action list-group-item-primary li-margin li-choice");
+        choiceLi.setAttribute("data-id", shuffledQuiz[i].id);
+        choiceLi.setAttribute("data-choice", shuffledQuiz[i].choices[j]);
+        choiceLi.setAttribute("data-index", i + 1);
+        // choiceLi.innerHTML = `<button class="btn btn-primary" id="${shuffledQuiz[i].id}_${shuffledQuiz[i].choices[j]}_${i + 1}" onclick="answerChecker(this.id)">${shuffleChoices[j]}</button>`;
+        // choiceLi.appendChild(choiceBtn)
         choiceUl.appendChild(choiceLi);
       }
       questionDiv.appendChild(choiceUl);
     }
-    orderCounter++;
   }
 };
 //Quiz
 
-
-
-quizMaker()
+//start button event
+startBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  quizMaker();
+  startDiv.setAttribute("style", "display:none;");
+  const questionOne = document.getElementById("question_1");
+  questionOne.setAttribute("style", "display:block;")
+});
+//start button event
